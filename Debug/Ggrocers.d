8c2770e933362a3ddf40f98d0a61bd8f88e0c12a
astfulin <275047590@qq.com>
2019-09-23 10:15:06 +0800

    测试

---
+++ Debug/Ggrocers.d
@@ -0,0 +1,11 @@
+# FIXED
+
+Ggrocers.obj: ../Ggrocers.c
+Ggrocers.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/stdio.h
+Ggrocers.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/linkage.h
+Ggrocers.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/stdarg.h
+
+../Ggrocers.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.4/include/stdarg.h: 
